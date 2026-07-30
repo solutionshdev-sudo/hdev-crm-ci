42062f5c12ae519156902e817ad14383440220e6
Harvey <solutionshdev@gmail.com>
2026-07-30 14:58:32 -0300

    Fase 6: ChatwootFbProvider e ChatwootDequeuedLogger
    As duas vivem dentro de config/initializers, que o Rails carrega direto -- nao
    passam pelo Zeitwerk, entao nao ha regra de arquivo casado com constante e nao
    ha git mv a fazer. Definicao e uso no mesmo arquivo, 2 refs cada.
    
    Vao no mesmo commit por serem a mesma categoria e do mesmo tamanho; as proximas
    (lib/ e app/) voltam a ser uma por commit, que e onde o Zeitwerk morde.

--- hdevCRM/config/initializers/facebook_messenger.rb
+++ hdevCRM/config/initializers/facebook_messenger.rb
@@ -1,5 +1,5 @@
 # ref: https://github.com/jgorset/facebook-messenger#make-a-configuration-provider
-class ChatwootFbProvider < Facebook::Messenger::Configuration::Providers::Base
+class HdevFbProvider < Facebook::Messenger::Configuration::Providers::Base
   CHANNEL_APP_SECRET_KEYS = %w[app_secret app_secret_key client_secret api_secret].freeze
 
   def valid_verify_token?(_verify_token)
@@ -44,7 +44,7 @@ end
 
 Rails.application.reloader.to_prepare do
   Facebook::Messenger.configure do |config|
-    config.provider = ChatwootFbProvider.new
+    config.provider = HdevFbProvider.new
   end
 
   Facebook::Messenger::Bot.on :message do |message|
